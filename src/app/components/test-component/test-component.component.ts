699a18a2eebf8a63200831f7e3777754717da5e0
Denis Yefremenko <denvirus83@mail.ru>
2024-01-25 16:19:49 +0100

    test: builder-test 2

--- src/app/components/test-component/test-component.component.ts
+++ src/app/components/test-component/test-component.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './test-component.component.html',
-  styleUrl: './test-component.component.scss'
+  styleUrl: './test-component.component.scss',
 })
 export class TestComponentComponent {
 
+  paragraph: string = 'This is a test component';
 }
